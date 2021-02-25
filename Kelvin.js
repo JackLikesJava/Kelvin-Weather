8ea6eab61d6c8994490257a09815f652d89772af
Jack Wiser <71061091+JackLikesJava@users.noreply.github.com>
2021-02-25 05:52:18 -0600

    Update Kelvin.js
    Finished product

--- Kelvin.js
+++ Kelvin.js
@@ -7,4 +7,8 @@ console.log(celsuisTemp);
 //I created a variable that takes the CelsuisTemp variable so that it can do the math to find out what the fahrenheitTtemp is
 let fahrenheit = celsuisTemp * (9/5) + 32;
 console.log(fahrenheit);
-//continue to the math.floor() portion when you return.
+//In the code below, it is rounding down to the nearest 
+fahrenheit = Math.floor(fahrenheit);
+//You need to do the back tics `` and ${} for the interpolation to work!
+console.log(`the temperature is ${fahrenheit} degrees Fahrenheit`);
+//now I can convert Celsius to Newton
